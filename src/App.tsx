4987e58de4c4da4c71d79b408bc5c3f22188fe4b
CiprianBoamba <ciprian.boamba@gmail.com>
2023-10-13 12:32:20 +0100

    clean files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Button } from '@material-tailwind/react';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-600">
-        Simple React Typescript Tailwind Sample
-      </h1>
-      <Button>Button</Button>
+      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+        <h1 className="text-3xl font-bold underline text-blue-600">
+          Simple React Typescript Tailwind Sample
+        </h1>
+        <Button>Button</Button>
+      </div>
     </div>
   );
 }
